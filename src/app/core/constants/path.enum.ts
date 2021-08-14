e153de9ea3cdf5ccece5d9e03860d97fd9682f97
Linh Doan <doanvulinh3108@gmail.com>
2021-08-14 15:25:34 +0700

    home layout structure

---
+++ src/app/core/constants/path.enum.ts
@@ -0,0 +1,11 @@
+export enum Path {
+  // General containers
+  Home = 'home',
+  NotFound = '404',
+  PermissionDenied = '403',
+
+  // Authentication
+  SignIn = 'sign-in',
+  SignUp = 'sign-up',
+  ForgotPassword = 'forgot-password',
+}
