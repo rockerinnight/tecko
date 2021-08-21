94c1ae9801c5d3f94f3e74f60d44c0fbe8767e09
Linh Doan <doanvulinh3108@gmail.com>
2021-08-21 10:11:46 +0700

    create search-box, restructure layout, styles

--- src/app/pages/layout/profile-card/profile-card.component.ts
+++ src/app/pages/layout/profile-card/profile-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile-card',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-card.component.scss'],
 })
 export class ProfileCardComponent implements OnInit {
+  @Input() layout!: string;
   onClick = false;
 
   constructor() {}
